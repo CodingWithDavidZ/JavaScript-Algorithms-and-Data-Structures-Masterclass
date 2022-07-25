4d56e6986aa045d5403b2910c6c5758f749f8711
David Zombar <davidzombar@gmail.com>
2022-07-25 17:02:01 -0600

    added example object

--- 2 Analyzing Performance of Arrays and Objects/Objects.js
+++ 2 Analyzing Performance of Arrays and Objects/Objects.js
@@ -3,3 +3,8 @@ let instructor = {
     isInstructor: true,
     favoriteNumbers: [1, 2, 3, 4]
 }
+
+console.log(Object.keys(instructor));
+console.log(Object.values(instructor));
+console.log(Object.entries(instructor));
+//The above are all Linear Time or O(n)
