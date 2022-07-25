aaab49ac89b23e21a8e086656a267480ff8c1a80
David Zombar <davidzombar@gmail.com>
2022-07-25 11:53:20 -0600

    added example of space complexity

---
+++ Big-O Notation/Space Complexity/spaceExample.js
@@ -0,0 +1,9 @@
+function sum(arr){
+    let total = 0
+    for(let i = 0; i < arr.length; i++){
+        total += arr[i]
+    }
+    return total
+}
+
+// O(1) because we only have 2 two variables as denoted by the "=" sign
