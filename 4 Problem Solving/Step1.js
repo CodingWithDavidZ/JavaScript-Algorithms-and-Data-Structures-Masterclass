d0941db0d648f78114d08dae13072abd01d40eab
David Zombar <davidzombar@gmail.com>
2022-08-17 16:07:40 -0600

    added md and step 1 example for problem solving section

---
+++ 4 Problem Solving/Step1.js
@@ -0,0 +1,23 @@
+// 1. Can I restate the problem in my own words?
+// 2. What are the inputs that go into the problem?
+// 3. What are the outputs that should come from the solution to the problem
+// 4. Can the outputs be determined from the inputs? Do I have enough information to solve the problem
+// 5. How should I label the important pieces of data that are a part of the problem?
+
+//example problem:
+//Write a Function that takes two Numbers and Returns their Sum
+
+//restate the problem:
+//implement addition function
+
+//Inputs:
+//likely two integers or floating point numbers, what if number is very large, or a string?
+
+//Outputs:
+//can be derived from inputs as long as inputs are valid
+
+//Labeling
+//input1: num1
+//input2: num2
+//output: sum
+//function: add2nums
---
+++ 4 Problem Solving/Steps For Problem Solving.md
@@ -0,0 +1,6 @@
+#Problem Solving Steps
+1. Understand the problem
+1. Concrete examples
+1. Break it down
+1. Solve/Simplify
+1. Look back and refactor
