63c3996b9891874cbc9f4b3e41dc7d3e02b0acdb
David Zombar <davidzombar@gmail.com>
2022-08-29 14:52:27 -0600

    adding logic

--- 4 Problem Solving/Step3.js
+++ 4 Problem Solving/Step3.js
@@ -12,3 +12,15 @@ function charCount(str){
     //do something
     // return an object with keys that are lowercase alphanumeric characters in the string; values should be the count for those characters
 }
+
+
+function charCount(str) {
+	//make object to return at end
+    //loop over string, for each character...
+        // if char is a number/letter AND is a key in object, add one to count
+        //if char is not a key in object AND is a number/letter, add it and set count to one.
+        //if char is something else (space, period, etc.) don't do anything.
+    //return object at end
+}
+
+charCount("Your PIN is 1234!")
