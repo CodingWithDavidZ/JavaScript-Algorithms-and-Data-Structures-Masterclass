c80d60fc829452df06f6c6bfdc8c31e7f080de90
David Zombar <davidzombar@gmail.com>
2022-08-29 14:47:29 -0600

    working on example for step 3

--- 4 Problem Solving/Step3.js
+++ 4 Problem Solving/Step3.js
@@ -1,1 +1,14 @@
 // Break it down
+//	Explicitly write out the steps you need to take
+//		 ○ Forces you to think about the code you'll write before you write it, 
+//         and helps you catch any lingering conceptual issues or misunderstandings
+//         before you dive in and have to worry about details as well.
+
+//Example:
+//Write a function which takes in a string and returns counts of each character in the string
+
+
+function charCount(str){
+    //do something
+    // return an object with keys that are lowercase alphanumeric characters in the string; values should be the count for those characters
+}
