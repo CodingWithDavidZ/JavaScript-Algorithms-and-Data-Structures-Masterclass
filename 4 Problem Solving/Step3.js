727948cc47830c2030f4a490025ad891184f49d8
David Zombar <davidzombar@gmail.com>
2022-08-30 16:40:33 -0600

    adding example

--- 4 Problem Solving/Step3.js
+++ 4 Problem Solving/Step3.js
@@ -19,7 +19,7 @@ function charCount(str) {
     //loop over string, for each character...
         // if char is a number/letter AND is a key in object, add one to count
         //if char is not a key in object AND is a number/letter, add it and set count to one.
-        //if char is something else (space, period, etc.) don't do anything.
+        //if char is something else (space, period, etc.) don't do anything
     //return object at end
 }
 
---
+++ 4 Problem Solving/Step4.js
@@ -0,0 +1,13 @@
+//Solve or Simplify
+
+//Example:
+//Write a function which takes in a string and returns counts of each character in the string
+
+function charCount(str) {
+	//make object to return at end
+	//loop over string, for each character...
+	// if char is a number/letter AND is a key in object, add one to count
+	//if char is not a key in object AND is a number/letter, add it and set count to one.
+	//if char is something else (space, period, etc.) don't do anything
+	//return object at end
+}
