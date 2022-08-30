554f2388caeb322570b5d12dc6822b677517960e
David Zombar <davidzombar@gmail.com>
2022-08-30 16:46:10 -0600

    working on example

--- 4 Problem Solving/Step4.js
+++ 4 Problem Solving/Step4.js
@@ -4,8 +4,13 @@
 //Write a function which takes in a string and returns counts of each character in the string
 
 function charCount(str) {
+    let lowercase = str.lowercase
 	//make object to return at end
+    let result = {}
 	//loop over string, for each character...
+    for(var i = 0; i < lowercase.length; i++){
+        
+    }
 	// if char is a number/letter AND is a key in object, add one to count
 	//if char is not a key in object AND is a number/letter, add it and set count to one.
 	//if char is something else (space, period, etc.) don't do anything
