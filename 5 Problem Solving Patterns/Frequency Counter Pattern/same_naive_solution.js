854bd6efef5f84bdecdd8080d68f8f8f15774eab
David Zombar <davidzombar@gmail.com>
2022-11-07 14:41:17 -0700

    commented code

--- 5 Problem Solving Patterns/Frequency Counter Pattern/same_naive_solution.js
+++ 5 Problem Solving Patterns/Frequency Counter Pattern/same_naive_solution.js
@@ -1,15 +1,18 @@
 
 
 function same(arr1, arr2) {
+	// if the two arrays have different lengths, return false because they cannot be the same
 	if (arr1.length !== arr2.length) {
 		return false;
 	}
 	for (let i = 0; i < arr1.length; i++) {
 		let correctIndex = arr2.indexOf(arr1[i] ** 2);
+		// if the correctIndex is -1, it means that the value is not in the array so return false
 		if (correctIndex === -1) {
 			return false;
 		}
 		console.log(arr2);
+		// remove the value from the array so that it is not counted again
 		arr2.splice(correctIndex, 1);
 	}
 	return true;
