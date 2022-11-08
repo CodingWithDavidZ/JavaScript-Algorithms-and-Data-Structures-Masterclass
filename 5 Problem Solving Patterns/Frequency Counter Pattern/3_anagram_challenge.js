60fb4898769a6c24e1d8f605fed52a44300247a1
David Zombar <davidzombar@gmail.com>
2022-11-08 10:05:19 -0700

    testing macro

---
+++ 5 Problem Solving Patterns/Frequency Counter Pattern/3_anagram_challenge.js
@@ -0,0 +1,6 @@
+// Create a validAnagram function that accepts two strings and returns true if the second string is an anagram of the first string. Otherwise, return false.
+
+
+git commit "test"
+
+//testing macro 
