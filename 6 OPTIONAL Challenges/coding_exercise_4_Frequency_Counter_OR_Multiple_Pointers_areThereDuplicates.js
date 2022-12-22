b189437abef5e535b7c4bfc535a2f143f8d778b6
David Zombar <davidzombar@gmail.com>
2022-12-22 15:03:27 -0700

    added comments and problem solving approach

--- 6 OPTIONAL Challenges/coding_exercise_4_Frequency_Counter_OR_Multiple_Pointers_areThereDuplicates.js
+++ 6 OPTIONAL Challenges/coding_exercise_4_Frequency_Counter_OR_Multiple_Pointers_areThereDuplicates.js
@@ -6,6 +6,47 @@
 // 	• areThereDuplicates(1, 2, 2) // true 
 // 	• areThereDuplicates('a', 'b', 'c', 'a') // true 
 
+// Problem Solving Approach:
+// Understanding the Problem:
+// 	1. Can I restate the problem in my own words:
+// 		○ Check a random number of given input for duplicates
+// 	2. What inputs go into the problem:
+// 		○ Int
+// 		○ String
+// 	3. What is the expected output:
+// 		○ Data type: Boolean
+// 		○ Example: True or False
+// 	4. Can the output be determined from the input:
+// 		○ Yes 
+// 	5. How should I label the data:
+// 		○ Function Names:
+// 			§ areThereDuplicates
+// 		○ Inputs:
+// 			§ args
+// 		○ Variables:
+// 			§ argObj
+// 		○ Outputs:
+// 			§ result
+// Concrete Examples:
+// 	1. Simple Examples:
+// 		○ Input: (3,2,1,2)
+// 			§  Output: True
+// 		○ Input: (3,2,1,4)
+// 			§  Output: False
+// 		○ Input: (a,c,e,t,e)
+// 			§  Output: True
+// 	2. Complex Examples:
+// 		○ Input: 
+// 			§  Output: 
+// 	3. Edge Cases: (NaN, Null, Error, Not Found, etc…)
+// 		○ check if result is boolean, if false, return "input error"
+// Break It Down Step-by-step:
+// 	•  Create an object that take each argument as a key and if the key doesn't exist in the object, initialize to one, and if it does exist ++
+// 	•  Check that objects values do not exceed 1
+// Can You Solve This Problem:
+// 	• Yes
+// Do it
+
 function areThereDuplicates() {
   //create one object to store all arguments where the key is the argument and the value is the number of times it appears
     let argObj = {};
@@ -29,6 +70,6 @@ function areThereDuplicates() {
         return false;
     }
 
-// areThereDuplicates(1, 2, 3) // false
-// areThereDuplicates(1, 2, 2) // true
-// areThereDuplicates('a', 'b', 'c', 'a') // true
+areThereDuplicates(1, 2, 3) // false
+areThereDuplicates(1, 2, 2) // true
+areThereDuplicates('a', 'b', 'c', 'a') // true
